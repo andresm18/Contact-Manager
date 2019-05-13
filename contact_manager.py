2c2405ce96fcfac9d7ca89488bb1609c509a6371
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2019-05-13 14:33:27 -0600

    función para añadir un contacto

--- contact_manager.py
+++ contact_manager.py
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
+### CODE
+
+##Fase 1:
 # Estructura de contactos
 contactos = [{"Nombre": [], "Apellido": [], "Teléfono": []}]
 
 
 # Función para añadir contacto 
 def add_contact(nombre, apellido, telefono):
-    new_contact = {}
-    return(add_contact)
-
-
-=======
-print("Commit")
-print ("Prueba")
->>>>>>> eb09b2df7413013a3184205c1a719f5a2533a9b0
+    contactos = ({"Nombre": nombre, "Apellido": apellido, "Teléfono": telefono})
+    print(contactos)
