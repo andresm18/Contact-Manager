672250de2fd92ac8322af40b6719aeddfef8ce00
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2019-05-13 16:44:38 -0600

    input datos

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import contact_manager
 print("")
 print("------ Contactos ------")
 print("\n")
-#print("Presione 1 para añadir un contacto ")
-nombre = input("Ingrese el nombre: ")
-apellido = input("Ingrese el apellido: ")
-telefono = input("Ingrese eñ telefono: ")
-print("\n")
-    
-contact_manager.add_contact(nombre,apellido,telefono)
+cant_new_contacts = int(input("Ingrese la cantidad de contactos que desea añadir: "))
 print("")
+
+for agregar in range(cant_new_contacts):
+    nombre = input("Ingrese el nombre: ")
+    apellido = input("Ingrese el apellido: ")
+    telefono = input("Ingrese el telefono: ")
+    print("\n")
+    contact_manager.add_contact(nombre, apellido, telefono)
+    print("\n\n")
+
