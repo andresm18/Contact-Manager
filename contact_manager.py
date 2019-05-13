e666519c5b6f445649e9faed38181ee107c56e93
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2019-05-13 13:02:34 -0600

    estructura de para los contactos

--- contact_manager.py
+++ contact_manager.py
@@ -1,2 +1,10 @@
-contactos = {"Nombre":{}, "Apellido":{}, "Teléfono": {}}
+# Estructura de contactos
+contactos = [{"Nombre": [], "Apellido": [], "Teléfono": []}]
+
+
+# Función para añadir contacto 
+def add_contact(nombre, apellido, telefono):
+    new_contact = {}
+    return(add_contact)
+
 
