dc3d7657116255412916cb9e73d1fa7f9cb2981e
katherineggs <katherinegarcia@ufm.edu>
2019-05-14 06:53:18 -0600

    <+ RemoveContact>

--- contact_manager.py
+++ contact_manager.py
@@ -7,10 +7,26 @@ contactos = []
 # Función para añadir contacto 
 def add_contact(nombre, apellido, telefono):
     contactos.append({"Nombre": nombre, "Apellido": apellido, "Teléfono": telefono})
-    print(contactos)
 
 def listcontacts():
 	print("Contactos:")
 	for i in contactos:
 		print("\n{}\n".format(i))
 listcontacts()
+
+#Remover contactos
+def remove_contact(nombre,apellido):
+	for i in contactos:
+		for (k,v) in i.items():
+			if v == nombre:
+				a = contactos.index(i)
+				print(a)
+			if v == apellido:
+				b = contactos.index(i)
+				print(b)
+				contactos.pop(b)
+	listcontacts()
+
+					
+			#if casilla == nombre:
+	
